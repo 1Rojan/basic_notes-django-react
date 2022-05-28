148cd3efae009be3d01bb1cd4d16079ac404d69a
1Rojan <rznsnr11@gmail.com>
2022-05-28 12:20:17 +0545

    created Note model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
+from statistics import mode
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+  body = models.TextField(null=True, blank=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  crated_at = models.DateTimeField(auto_now_add=True)
+
+
+  def __str__(self):
+    return self.body[:50]
